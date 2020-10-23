de195332cef3b9153f4dc4556d19b0f75abdc47b
jimmyjongs <jdojimmy@gmail.com>
2020-10-23 14:38:19 -0700

    add appointment creation frontend

---
+++ etoot-front-end/src/components/tutor-list.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+export default class TutorList extends Component{
+    render(){
+        return(
+            <div>
+                <p>
+                    You are on the tutor list.
+                </p>
+            </div>
+        )
+    }
+}
