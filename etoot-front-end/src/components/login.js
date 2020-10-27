4696a9a0bceeeae016a9a9853f9f46a5a28a23ac
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-26 21:24:00 -0700

    login frontend
    login-feature branch

--- etoot-front-end/src/components/login.js
+++ etoot-front-end/src/components/login.js
@@ -1,13 +1,34 @@
-import React, {Component} from 'react'
-
+import React, {Component, useState} from 'react'
+import './login.css'
 
 export default class Login extends Component{
     render(){
         return(
-            <div>
-                <p>
-                    You are on the login route.
-                </p>
+            <div className="login">
+                <img 
+                    className="login__logo"
+                    src="https://i.ibb.co/dGhtVsZ/Etoot-logo.jpg"
+                />
+
+                <div className='login__container'>
+                    <h1>Sign-in</h1>
+
+                    <form>
+                        <h5>E-mail</h5>
+                        <input type='text' />
+
+                        <h5>Password</h5>
+                        <input type='password'/>
+
+                        <button type='submit' className='login__signInButton'>Sign In</button>
+                    </form>
+
+                    <p>
+                        By signing-in you agree to the Etoot Conditions of Use.
+                    </p>
+
+                    <button className='login__registerButton' >Create your Etoot Account</button>
+                </div> 
             </div>
         )
     }
