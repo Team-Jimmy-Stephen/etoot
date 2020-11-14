acb564b44d49aadc4c4cdb0b121e7939dd1544d2
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-11-13 17:19:23 -0800

    header done

---
+++ etoot-front-end/src/components/landingpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import Header from './landingHeader.js';
+
+class landingpage extends Component {
+    render() {
+        return (
+            <div class="main-container">
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default landingpage;
