f3dac67b2fc2ef1fecf60c38b261351ced918ddf
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-11-13 20:09:11 -0800

    complete landing page

---
+++ frontend/src/components/landingpage.js
@@ -0,0 +1,19 @@
+  
+import React, { Component } from "react"
+import Header from './landingHeader.js';
+import Content from './landingContent.js'
+import Footer from './landingFooter.js'
+
+class landingpage extends Component {
+    render() {
+        return (
+            <div class="main-container">
+                <Header />
+                <Content />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default landingpage;
