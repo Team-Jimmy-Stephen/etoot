d1a5bc3faef75b323e66548ee5baf767182a5eac
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-11-13 19:21:40 -0800

    landing page done

--- etoot-front-end/src/components/landingHeader.js
+++ etoot-front-end/src/components/landingHeader.js
@@ -4,12 +4,10 @@ function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light static-top header-a">
             <div className="container nav-container">
-                <a className="navbar-brand brand" href="#">Etoot</a>
-
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
+                <br />
+                <br />
+                <br />
+                <a className="navbar-brand brand" href="/">Etoot</a>
                 <div className="collapse navbar-collapse alink" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                         <a className="btn btn-outline-dark start" href="Home">Get Started</a>
--- etoot-front-end/src/components/landingpage.js
+++ etoot-front-end/src/components/landingpage.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react"
 import Header from './landingHeader.js';
+import Content from './landingContent.js'
+import Footer from './landingFooter.js'
 
 class landingpage extends Component {
     render() {
         return (
             <div class="main-container">
                 <Header />
+                <Content />
+                <Footer />
             </div>
         )
     }
